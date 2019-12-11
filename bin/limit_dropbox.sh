4096e01a49709db101f717a10faf813270241915
kyamaguchi <kzh.yap@gmail.com>
2019-12-11 09:43:50 +0900

    Add a script to limit the cpu usage of dropbox

---
+++ bin/limit_dropbox.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cpulimit -p $(ps -A | grep " /Applications/Dropbox.app/Contents/MacOS/Dropbox$" | awk '{print $1}') -l 60 &
+
+# Refs:
+# $ brew install cpulimit
+# https://github.com/Homebrew/homebrew-core/blob/master/Formula/cpulimit.rb
+# https://medium.com/@sbr464/limit-dropbox-and-others-on-macos-from-taking-100-cpu-877266df104d
+#   -> cputhrottle didn't work
